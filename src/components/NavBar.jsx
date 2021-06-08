a6fdb64b4ac31e3a4d537c2ee0e8b0d2bfe6e817
streppelandres <andres.streppel@gmai.com>
2021-06-08 02:46:28 -0300

    Desafío 2: Menú e-commerce

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav className='nav-bar m-l_a p-tb_22'>
+            <a href='/'>Inicio</a>
+            <a href='/'>Categorías</a>
+            <a href='/'>Ofertas</a>
+            <a href='/'>Ayuda</a>
+        </nav>
+    )
+};
+
+export default NavBar;
