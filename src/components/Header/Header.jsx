ff36595cceb3a89c3a0ddd38e6679b2c3bd7a6e7
streppelandres <andres.streppel@gmai.com>
2021-06-26 19:48:57 -0300

    Actualizo header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,14 @@
-import { NavBar } from "../NavBar/NavBar"
 import './Header.css';
+import { Logo } from "../Logo/Logo";
+import { NavBar } from "../NavBar/NavBar";
+import { GameQuotes } from '../GameQuotes/GameQuotes';
 
 export const Header = () => {
     return (
         <header>
             <div className="basic-container d-f a-i_c">
-                <div className="logo-container">
-                    <img className="isologo" src={process.env.PUBLIC_URL + 'logo.png'} alt="logo" />
-                    <div className="logo-text">
-                        <h2>NivelX</h2>
-                        <span>Club</span>
-                    </div>
-                </div>
-
-                <img className="quotes" src={process.env.PUBLIC_URL + 'games-quotes.gif'} alt="quotes" />
+                <Logo/>
+                <GameQuotes/>
                 <NavBar/>
             </div>
         </header>
