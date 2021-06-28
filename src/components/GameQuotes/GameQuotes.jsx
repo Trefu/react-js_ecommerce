31d8c4f214fc8da0e6d03ab74006655a8cc5471d
streppelandres <andres.streppel@gmai.com>
2021-06-28 00:17:16 -0300

    Quito código estático de dentro del componente

--- src/components/GameQuotes/GameQuotes.jsx
+++ src/components/GameQuotes/GameQuotes.jsx
@@ -1,6 +1,12 @@
 import "./GameQuotes.css";
 import React, { useState, useEffect } from 'react';
 
+const IMG_PATH = process.env.PUBLIC_URL + 'quotes/';
+const QUOTES_AND_IMAGES = [
+    ['¡Detrás de ti, imbecil!', 'Wololo!', 'Mrglglglgl', 'Go go go!', 'Waka waka', '¡Solo teníamos que seguir al Maldito Tren, CJ!', 'Thank you!'],
+    ['rs4', 'monje', 'murloc', 'ct' , 'pacman', 'smoke', 'tnkyou']
+];
+
 /**
  * Cambia las citas de los videojuegos utilizando un contador y haciendo provecho de useEffect
  * @todo: Hacer que revise la cpartea /quotes y detecte automaticamente las imagenes nombradas con su respectiva frase
@@ -9,11 +15,6 @@ import React, { useState, useEffect } from 'react';
  */
 export const GameQuotes = () => {
     const [counter, setCounter] = useState(1);
-    const IMG_PATH = process.env.PUBLIC_URL + 'quotes/';
-    const QUOTES_AND_IMAGES = [
-        ['¡Detrás de ti, imbecil!', 'Wololo!', 'Mrglglglgl', 'Go go go!', 'Waka waka', '¡Solo teníamos que seguir al Maldito Tren, CJ!', 'Thank you!'],
-        ['rs4', 'monje', 'murloc', 'ct' , 'pacman', 'smoke', 'tnkyou']
-    ];
 
     useEffect(() => {
         setTimeout(() => counter < QUOTES_AND_IMAGES[0].length - 1 ? setCounter(counter+1) : setCounter(0), 3 * 1000);
