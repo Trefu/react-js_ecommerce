ae37a9d22886a01eba02c01df7d1360cf9795072
streppelandres <andres.streppel@gmai.com>
2021-07-10 03:41:22 -0300

    Modifico el ItemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,14 +1,38 @@
+import { Link } from 'react-router-dom';
 import './ItemCount.css';
 
-export const ItemCount = ({ title, count, onAdd }) => {
+/**
+ * Hay algo que hace ruido.
+ * - Cuando se agrega al carrito, en realidad se está modificando la cantidad de items
+ * - No es que se agregan "X" más items, o se borran "X" items
+ * - Supongo que cuando controles stock, después vas a poder actualizarlo y va quedar más en claro
+ */
+
+const TXT_BTN_AGREGAR = 'Añadir al carrito';
+const TXT_BTN_ELIMINAR = 'Eliminar del carrito';
+
+export const ItemCount = ({ initial, stock, count, updateCount, addToCart }) => {
+    const BTN_ADD = document.querySelector('button.button-add');
+
+    const onUpdateCountHandler = (n) => {
+        BTN_ADD.style.display = "block";
+        changeButtonText(n <= 0 ? TXT_BTN_ELIMINAR : TXT_BTN_AGREGAR);
+        updateCount(n);
+    }
+
+    const changeButtonText = (text) => BTN_ADD.innerText = text;
+
     return (
         <div className="item-count">
-            {title ? <h4>{title}</h4> : <></>}
+
             <div className="button-container">
-                <button onClick={() => onAdd(count - 1)}>-</button>
-                <p>{count}</p>
-                <button onClick={() => onAdd(count + 1)}>+</button>
+                <button onClick={() => { onUpdateCountHandler(count - 1); }}>-</button>
+                <p>{count > 0 ? count : initial} <span> de {stock}</span></p>
+                <button onClick={() => { onUpdateCountHandler(count + 1); }}>+</button>
             </div>
+
+            <button style={{ display: "none" }} className="button-add m-t_16" onClick={addToCart}>{initial === 0 ? TXT_BTN_ELIMINAR : TXT_BTN_AGREGAR}</button>
+            <Link className="m_0" to={'/cart'}>Ver carrito</Link>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,16 @@
 import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext/CartContext';
 import { ItemCount } from '../ItemCount/ItemCount';
 import './ItemDetail.css';
 
-const ITEM_STOCK = 99;
+const MOCK_ITEM_STOCK = 5;
 
 export const ItemDetail = ({ item, description }) => {
     const { addItem } = useContext(CartContext);
-    const [count, setCount] = useState(1);
-    const onAdd = (n) => n <= ITEM_STOCK && n >= 0 ? setCount(n) : '';
-
+    const [count, setCount] = useState(0);
+    const updateCount = (n) => n <= MOCK_ITEM_STOCK && n >= 0 ? setCount(n) : '';
+    const addToCart = () => addItem(item, count);
+    
     return (
         <div className="item-detail p_32 bdr-g-l_3 bg-g-l_1 b-r_5 d-f a-i_c row-to-column_in-980">
             <img src={item.pictures[0].url} alt="" />
@@ -18,15 +18,7 @@ export const ItemDetail = ({ item, description }) => {
                 <h1 className="m_0 m-b_16">{item.title}</h1>
                 <h3 className="m_0 ars-symbol">{item.price}</h3>
                 <p>{description}</p>
-
-                 {/*
-                    - Quedó el ejercicio anterior mezclado con este.
-                    - Se utiliza el contador para enviarle al context la cantidad.
-                 */}
-                
-                <button onClick={() => { addItem(item, count) }} className="btn-primary m-t_16">Comprar</button>
-
-                {count === ITEM_STOCK ? <Link className="btn-primary m-t_16" to={`/cart`}>Terminar compra</Link> : <ItemCount count={count} onAdd={onAdd} />}
+                <ItemCount initial={0} stock={MOCK_ITEM_STOCK} count={count} updateCount={updateCount} addToCart={addToCart} />
             </div>
         </div>
     )
