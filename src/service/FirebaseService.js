7ee35a82a627ac7c361676b99e34745b19574782
streppelandres <andres.streppel@gmai.com>
2021-07-31 03:31:52 -0300

    Firebase service: Agrego método para actualizar item y otros ajustes

--- src/service/FirebaseService.js
+++ src/service/FirebaseService.js
@@ -10,10 +10,30 @@ export const firebaseService = {
     /**
      * Para buscar en la base de datos de Firebase
      * @param {*} nameCollection Nombre de la colección a encontrar
+     * @return {*} Elementos encontrados
+     */
+    findAll: async (nameCollection) => {
+        return await utilMapResponse(await FIRESTORE.collection(nameCollection).get());
+    },
+
+    /**
+     * Para buscar en la base de datos de Firebase
+     * @param {*} nameCollection Nombre de la colección a encontrar
      * @param {*} setterCallback Callback del setter para guardar el resultado
      */
-    findAll: async (nameCollection, setterCallback) => {
-        setterCallback(await utilMapResponse(await FIRESTORE.collection(nameCollection).get()));
+    findAndSetAll: async (nameCollection, setterCallback) => {
+        console.log(nameCollection, setterCallback);
+        setterCallback(await firebaseService.findAll(nameCollection));
+    },
+
+    /**
+     * Para buscar con filtro en la base de datos de Firebase
+     * @param {*} nameCollection Nombre de la colección a encontrar
+     * @param {*} whereFilter Filtro para el where, ej: ['price', '>', 100]
+     * @return {*} Elementos encontrados
+     */
+    findWithFilter: async (nameCollection, whereFilter) => {
+        return utilMapResponse(await FIRESTORE.collection(nameCollection).where(...whereFilter).get());
     },
 
     /**
@@ -22,30 +42,50 @@ export const firebaseService = {
      * @param {*} setterCallback Callback del setter para guardar el resultado
      * @param {*} whereFilter Filtro para el where, ej: ['price', '>', 100]
      */
-    findWithFilter: async (nameCollection, setterCallback, whereFilter) => {
-        setterCallback(await utilMapResponse(await FIRESTORE.collection(nameCollection).where(...whereFilter).get()));
+    findAndSetWithFilter: async (nameCollection, setterCallback, whereFilter) => {
+        setterCallback(firebaseService.findWithFilter(nameCollection, whereFilter));
+    },
+
+    /**
+     * Para buscar por id de documento
+     * @param {*} nameCollection Nombre de la colección a buscar
+     * @param {*} id Id del item a buscar
+     * @return {*} Elemento encontrado
+     */
+    findById: async (nameCollection, id) => {
+        const r = await FIRESTORE.collection(nameCollection).doc(id).get();
+        return { id: r.id, ...r.data() };
     },
 
     /**
      * Para buscar por id de documento
      * @param {*} nameCollection Nombre de la colección a buscar
      * @param {*} setterCallback Callback del setter para guardar el resultado
-     * @param {*} id Id del item a buscar
+     * @param {*} id Del item a buscar
      */
-    findById: async (nameCollection, setterCallback, id) => {
-        const r = await FIRESTORE.collection(nameCollection).doc(id).get();
-        console.log(`Se encontró el item ${r.data().name}`);
-        setterCallback({ id:r.id, ...r.data() })
+    findAndSetById: async (nameCollection, setterCallback, id) => {
+        setterCallback(firebaseService.findById(nameCollection, id));
     },
 
     /**
      * Para agregar un item a una colección
      * @param {*} nameCollection Nombre de la colección en donde guardar el item
      * @param {*} item Item a guardar
+     * @return {*} id del nuevo item
      */
     addItem: async (nameCollection, item) => {
-        const r = await FIRESTORE.collection(nameCollection).add(item);
-        console.log(`Se agregó nuevo item con la id [${r.id}] a la colección [${nameCollection}]`);
+        const { id } = await FIRESTORE.collection(nameCollection).add(item);
+        return id;
+    },
+
+    /**
+     * Actualiza un item
+     * @param {*} nameCollection Nombre de la colección en donde esta el item
+     * @param {*} id Del item a actualizar
+     * @param {*} fieldsObj Campos a actualizar ej: { stock : 2500, price : 2500 }
+     */
+    updateItem: async (nameCollection, id, fieldsObj) => {
+        await FIRESTORE.collection(nameCollection).doc(id).update(fieldsObj);
     }
 
 }
