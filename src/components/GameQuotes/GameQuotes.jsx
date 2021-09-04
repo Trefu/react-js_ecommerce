872b5a40687719488c8fbb96f976711fdde1d075
Lucas Leonel Lopez <tre.lopez@hotmail.com>
2021-09-03 22:45:43 -0300

    added padding clases,container mediaquerys, fixed responsive mobile

--- src/components/GameQuotes/GameQuotes.jsx
+++ src/components/GameQuotes/GameQuotes.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 const IMG_PATH = '/quotes/';
 const QUOTES_AND_IMAGES = [
     ['¡Detrás de ti, imbecil!', 'Wololo!', 'Mrglglglgl', 'Go go go!', 'Waka waka', '¡Solo teníamos que seguir al Maldito Tren, CJ!', 'Thank you!'],
-    ['rs4', 'monje', 'murloc', 'ct' , 'pacman', 'smoke', 'tnkyou']
+    ['rs4', 'monje', 'murloc', 'ct', 'pacman', 'smoke', 'tnkyou']
 ];
 
 /**
@@ -17,13 +17,13 @@ export const GameQuotes = () => {
     const [counter, setCounter] = useState(1);
 
     useEffect(() => {
-        setTimeout(() => counter < QUOTES_AND_IMAGES[0].length - 1 ? setCounter(counter+1) : setCounter(0), 3 * 1000);
+        setTimeout(() => counter < QUOTES_AND_IMAGES[0].length - 1 ? setCounter(counter + 1) : setCounter(0), 3 * 1000);
     }, [counter]);
 
     return (
         <div className="gamequotes m-l_32">
             <img src={IMG_PATH + QUOTES_AND_IMAGES[1][counter] + '.png'} alt="quotes" />
-            <p>{'“' + QUOTES_AND_IMAGES[0][counter] + '”'}</p>
+            <p className='text-xs'>{'“' + QUOTES_AND_IMAGES[0][counter] + '”'}</p>
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -9,16 +9,16 @@ export const ItemDetail = ({ item, description }) => {
     const [count, setCount] = useState(obtenerCantidadDeUnItemById(item.id));
     const updateCount = (n) => n <= item.stock && n > 0 ? setCount(n) : '';
     const addToCart = () => addItem(item, count);
-    
-    return (
-        <div className="item-detail p_32 bdr-g-l_3 bg-g-l_1 b-r_5 d-f a-i_c row-to-column_in-980">
 
-            <div className="d-f f-d_c j-c_c t-a_c">
-                <img className="b-r_5p" src={item.img_url} alt="" />
+    return (
+        <div className="item-detail p_32 bdr-g-l_3 bg-g-l_1 b-r_5 d-f a-i_c row-to-column_in-980 m-b_32">
+
+            <div className="d-f f-d_c j-c_c t-a_c p-x_32 ">
+                <img className="b-r_5p w-100p" src={item.img_url} alt={item.name} />
                 <Link className="a-small m-l_a m-r_a m-t_12" to={`/add/${item.id}`}>Editar producto</Link>
             </div>
 
-            <div className="d-f f-d_c m-l_32">
+            <div className="d-f f-d_c ">
                 <h1 className="m_0 m-b_16">{item.name}</h1>
                 <h3 className="m_0 ars-symbol">{item.price}</h3>
                 <ItemCount initial={count} stock={item.stock} count={count} updateCount={updateCount} addToCart={addToCart} />
